c8a3f2fc1ffc84a00ee1035f541b3518775aa675
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-18 11:18:52 +0200

    add matchMedia for widnow 768px mob-menu.js

--- src/scripts/mob-menu.js
+++ src/scripts/mob-menu.js
@@ -1,11 +1,25 @@
-const mobMenuBtn = document.querySelector(".mob-menu");
-const mobMenu = document.querySelector(".mob-nav");
+const documentEl = {
+  mobMenuBtn: document.querySelector(".mob-menu"),
+  mobMenu: document.querySelector(".mob-nav"),
+  mobMenuOpenSVG: document.querySelector(".mob-menu__open"),
+  mobMenuCloseSVG: document.querySelector(".mob-menu__close"),
+};
 
-const mobMenuOpenSVG = document.querySelector(".mob-menu__open");
-const mobMenuCloseSVG = document.querySelector(".mob-menu__close");
+documentEl.mobMenuBtn.addEventListener("click", () => {
+  documentEl.mobMenu.classList.toggle("open");
+  documentEl.mobMenuOpenSVG.classList.toggle("visible");
+  documentEl.mobMenuCloseSVG.classList.toggle("visible");
 
-mobMenuBtn.addEventListener("click", () => {
-  mobMenu.classList.toggle("open");
-  mobMenuOpenSVG.classList.toggle("toggle");
-  mobMenuCloseSVG.classList.toggle("toggle");
+  documentEl.mobMenu.classList.contains("open")
+    ? (document.body.style.overflow = "hidden")
+    : document.body.style.removeProperty("overflow");
+});
+
+window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  if (!e.matches) return;
+
+  documentEl.mobMenu.classList.remove("open");
+  document.body.style.removeProperty("overflow");
+  documentEl.mobMenuOpenSVG.classList.toggle("visible");
+  documentEl.mobMenuCloseSVG.classList.toggle("visible");
 });
