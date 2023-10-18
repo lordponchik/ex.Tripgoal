2492b9342231fc3b289284690119505712042afd
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-18 15:35:26 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -117,62 +117,33 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+})({"scripts/mob-menu.js":[function(require,module,exports) {
+var documentEl = {
+  mobMenuBtn: document.querySelector(".mob-menu"),
+  mobMenu: document.querySelector(".mob-nav"),
+  mobMenuOpenSVG: document.querySelector(".mob-menu__open"),
+  mobMenuCloseSVG: document.querySelector(".mob-menu__close")
+};
+documentEl.mobMenuBtn.addEventListener("click", function () {
+  documentEl.mobMenu.classList.toggle("open");
+  documentEl.mobMenuOpenSVG.classList.toggle("visible");
+  documentEl.mobMenuCloseSVG.classList.toggle("visible");
+  documentEl.mobMenu.classList.contains("open") ? document.body.style.overflow = "hidden" : document.body.style.removeProperty("overflow");
+});
+window.matchMedia("(min-width: 768px)").addEventListener("change", function (e) {
+  if (!e.matches) return;
+  if (documentEl.mobMenu.classList.contains("open")) {
+    documentEl.mobMenuOpenSVG.classList.toggle("visible");
+    documentEl.mobMenuCloseSVG.classList.toggle("visible");
   }
-  return bundleURL;
-}
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-  return '/';
-}
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
-}
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-function updateLink(link) {
-  var newLink = link.cloneNode();
-  newLink.onload = function () {
-    link.remove();
-  };
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-var cssTimeout = null;
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-    cssTimeout = null;
-  }, 50);
-}
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"scss/index.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./../image/chevron-down.svg":[["chevron-down.d6648fc9.svg","image/chevron-down.svg"],"image/chevron-down.svg"],"./../image/map.svg":[["map.1cb1e9b8.svg","image/map.svg"],"image/map.svg"],"./../image/travel-guideline.jpg":[["travel-guideline.ae44267a.jpg","image/travel-guideline.jpg"],"image/travel-guideline.jpg"],"./../image/testimonials/map-world.svg":[["map-world.85ccb6c4.svg","image/testimonials/map-world.svg"],"image/testimonials/map-world.svg"],"./../image/contact-us.png":[["contact-us.1aeac65e.png","image/contact-us.png"],"image/contact-us.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  documentEl.mobMenu.classList.remove("open");
+  document.body.style.removeProperty("overflow");
+});
+},{}],"index.js":[function(require,module,exports) {
+"use strict";
+
+require("./scripts/mob-menu.js");
+},{"./scripts/mob-menu.js":"scripts/mob-menu.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -341,5 +312,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/scss.6b84482b.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
+//# sourceMappingURL=/src.e31bb0bc.js.map
