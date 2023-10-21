f546acdde02aa22c9111a90face4e3a3d2ba20a8
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-21 18:09:49 +0200

    add fn instRenderMob in instagram.js

--- src/scripts/instagram.js
+++ src/scripts/instagram.js
@@ -1,6 +1,41 @@
 import { instagramData } from "./instagramData.js";
+// import Swiper JS
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+// import Swiper styles
+import "swiper/swiper-bundle.css";
+import "swiper/modules/navigation.css";
 
-const instagrammListEl = document.querySelector(".instagramm__list");
+const instagrammListEl = document.querySelector(".instagram__wrapper");
+
+if (window.innerWidth >= 768) {
+  const instagramUlEl = document.createElement("ul");
+  instagramUlEl.classList.add("instagramm__list");
+  instagramUlEl.classList.add("list");
+
+  instagrammListEl.append(instagramUlEl);
+  instagramUlEl.innerHTML = instRenderTab(instagramData);
+} else if (window.innerWidth < 768) {
+  instagrammListEl.innerHTML = `<div class="swiper">
+  <div class="swiper-wrapper"></div>
+   <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+  </div>`;
+
+  const swiperEl = document.querySelector(".swiper-wrapper");
+
+  swiperEl.innerHTML = instRenderMob(instagramData);
+
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation],
+    direction: "horizontal",
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
 
 function instRenderTab(data) {
   return data
@@ -18,8 +53,39 @@ function instRenderTab(data) {
     .join("");
 }
 
-// function instRenderMob(data) {}
+function instRenderMob(data) {
+  return data
+    .map(({ name, alt }) => {
+      return `<div class="swiper-slide">
+              <img
+                src="${name}"
+                alt="${alt}"
+                class="swiper__img"
+                height="300"
+                width="400"
+              />
+            </div>`;
+    })
+    .join("");
+}
+{
+  /* <div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    ...
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
 
-if (window.innerWidth >= 768) {
-  instagrammListEl.innerHTML = instRenderTab(instagramData);
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div> */
 }
