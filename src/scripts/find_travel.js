a50f0e21ad078410608adefc42ca4add0fcd7290
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-25 13:37:45 +0200

    add fn stepsTabData in find_travel.js

--- src/scripts/find_travel.js
+++ src/scripts/find_travel.js
@@ -1,20 +1,49 @@
 import { descrImg } from "./find_travelData";
-
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+// import Swiper styles
+import "swiper/swiper-bundle.css";
+// import "swiper/modules/navigation.css";
 const stepsEl = document.querySelector(".steps");
 
 if (window.innerWidth >= 768) {
   stepsRenderTab();
 }
 
+function stepsRenderMob() {
+  stepsEl.innerHTML = `<div class="swiper">
+  <div class="swiper-wrapper"></div>
+   <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+  </div>`;
+}
 function stepsRenderTab() {
   const stepsListEl = document.createElement("ul");
   stepsListEl.classList.add("steps__list");
   stepsEl.appendChild(stepsListEl);
 
-  stepsListEl.innerHTML = stepsData(descrImg);
+  stepsListEl.innerHTML = stepsTabData(descrImg);
 }
 
-function stepsData(descr) {
+function stepsTabData(descr) {
+  return descr
+    .map(({ src, alt, title, descr }) => {
+      return `<div class="swiper-slide">
+              <img
+                  src="${src}"
+                  alt="${alt}"
+                  width="100"
+                  class="steps__img"
+                />
+                <h3 class="steps__title">${title}</h3>
+                <p class="steps__description">
+                  ${descr}
+                </p>
+            </div>`;
+    })
+    .join("");
+}
+function stepsMobData(descr) {
   return descr
     .map(({ src, alt, title, descr }) => {
       return `<li class="steps__item">
