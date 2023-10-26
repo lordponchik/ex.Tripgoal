07d8bffe126c3be9d47b95d2766c48a6a87e7678
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-26 17:46:20 +0200

    change reviewsData.review in testimonialsData

--- src/scripts/testimonialsData.js
+++ src/scripts/testimonialsData.js
@@ -15,54 +15,48 @@ export const reviewsData = [
     alt: "Buck Gallagher",
     activity: "Traveller",
     review:
-      "I have traveled and explored several destinations using this platform, no doubt it is highly recommended for traveling because it is comfortable and the service is great.",
+      "I am not a great book, I am not a great artist, but I love art and I love food, so I am the perfect traveller.",
   },
   {
     name: "Agnes Dalton",
     src: foto["agnes-dalton"],
     alt: "Agnes Dalton",
     activity: "Traveller",
-    review:
-      "I have traveled and explored several destinations using this platform, no doubt it is highly recommended for traveling because it is comfortable and the service is great.",
+    review: "He who does not travel does not know the value of men.",
   },
   {
     name: "Jeffry Johns",
     src: foto["jeffry-johns"],
     alt: "Jeffry Johns",
     activity: "Traveller",
-    review: `I have traveled and explored several destinations using this platform, no doubt it is highly 
-      recommended for traveling because it is comfortable and the service is great.`,
+    review: `The best journeys in life are those that answer questions you never thought to ask.`,
   },
   {
     name: "Joan Underwood",
     src: foto["joan-underwood"],
     alt: "Joan Underwood",
     activity: "Traveller",
-    review: `I have traveled and explored several destinations using this platform, no doubt it is highly 
-      recommended for traveling because it is comfortable and the service is great.`,
+    review: `A person does not grow from the ground like a vine or a tree, one is not part of a plot of land. Mankind has legs so it can wander.`,
   },
   {
     name: "Chad Wilson",
     src: foto["chad-wilson"],
     alt: "Chad Wilson",
     activity: "Traveller",
-    review: `I have traveled and explored several destinations using this platform, no doubt it is highly 
-      recommended for traveling because it is comfortable and the service is great.`,
+    review: `Certainly, travel is more than the seeing of sights; it is a change that goes on, deep and permanent, in the ideas of living.`,
   },
   {
     name: "Michael Small",
     src: foto["michael-small"],
     alt: "Michael Small",
     activity: "Traveller",
-    review: `I have traveled and explored several destinations using this platform, no doubt it is highly 
-      recommended for traveling because it is comfortable and the service is great.`,
+    review: `He who is outside his door has the hardest part of his journey behind him.`,
   },
   {
     name: "Shana Henry",
     src: foto["shana-henry"],
     alt: "Shana Henry",
     activity: "Traveller",
-    review: `I have traveled and explored several destinations using this platform, no doubt it is highly 
-      recommended for traveling because it is comfortable and the service is great.`,
+    review: `Travel is fatal to prejudice, bigotry, and narrow-mindedness, and many of our people need it sorely on these accounts. Broad, wholesome, charitable views of men and things cannot be acquired by vegetating in one little corner of the earth all one’s lifetime.`,
   },
 ];
